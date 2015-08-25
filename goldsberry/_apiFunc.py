7250c405ad9d4513736920a29c78d046f84eccde
Bradley Fay <bradley.fay@gmail.com>
2015-08-24 20:24:57 -0400

    visual updates

--- goldsberry/_apiFunc.py
+++ goldsberry/_apiFunc.py
@@ -234,7 +234,7 @@ def _valiDate(date_text):
             return str(date.date())
         except ValueError:
             raise ValueError("Incorrect data format, should be YYYY-MM-DD or MM-DD-YYYY")
-    else: pass
+    else: return ''
 def _ContextMeasure(x):
     measure = { 1:'FGM',2:'FGA',3:"FG_PCT",4:'FG3M',5:'FG3A',6:'FG3_PCT',8:'PF',
                 9:'EFG_PCT',10:'TS_PCT',11:'PTS_FB',12:'PTS_OFF_TOV',
